a94bd45e518c4723e2c163e9ad1d1dd1fc0a996f
Hamza Ar <hamza_razeq@hotmail.co.uk>
2019-05-21 12:23:34 +0100

    Functionality improved

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,14 +5,22 @@ public class App {
 		System.out.println("Hello World to the standard out");
 		Service service = new Service();
 		
-		Account acc = new Account("Hamza","Razeq",1);
+		Account acc = new Account("Hamza","Razeq");
+		Account acc2 = new Account("Ali","Azi");
 		service.addAccount(acc);
+		service.addAccount(acc2);
+		
 		System.out.println(service.getAccount(1).getFirstName());
+		System.out.println(acc.getAccountNumber());
+		
+		System.out.println(service.getAccount(2).getFirstName());
+		System.out.println(acc2.getAccountNumber());
 	}
 
 	public static void main(String[] args) {
 		new App();
 
 	}
+
 
 }
