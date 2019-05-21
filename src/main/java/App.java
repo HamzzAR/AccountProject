a537f1fe7714ccb41de752d0812d09439eb56f30
Hamza Ar <hamza_razeq@hotmail.co.uk>
2019-05-21 11:44:29 +0100

    Task done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,11 @@ public class App {
 
 	public App() {
 		System.out.println("Hello World to the standard out");
+		Service service = new Service();
+		
+		Account acc = new Account("Hamza","Razeq",1);
+		service.addAccount(acc);
+		System.out.println(service.getAccount(1).getFirstName());
 	}
 
 	public static void main(String[] args) {
